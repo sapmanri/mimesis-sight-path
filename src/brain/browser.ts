1ad6bda06c2faa0b3b5e8a3ae39ef8f5c06f13a7
sapmanri <byulsarang@gmail.com>
2026-07-13 15:50:14 +0900

    BUILD 405-E: export shared Habit storage and Byeoli calendar

--- src/brain/browser.ts
+++ src/brain/browser.ts
@@ -7,12 +7,29 @@ export {
   createEmptyHabitState,
 } from './habitEngine';
 
+export {
+  SHARED_HABIT_STORAGE_KEY,
+  LEGACY_HABIT_STORAGE_KEYS,
+  createSharedHabitStorage,
+  createSharedHabitEngine,
+} from './sharedHabit';
+
+export {
+  BYEOLI_DAY_SECONDS,
+  BYEOLI_CALENDAR_EPOCH_MS,
+  byeoliDayEpoch,
+  byeoliDate,
+  formatByeoliDate,
+} from './byeoliTime';
+
 export type {
   HabitRecord,
   HabitSnapshot,
   HabitState,
   HabitStorage,
 } from './habitEngine';
+
+export type { ByeoliDate } from './byeoliTime';
 
 export type {
   ByeoliActionIntent,
